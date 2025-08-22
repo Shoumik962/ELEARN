36638be41830c0d4a1e599c273f22ab7b8becebd
Shoumik962 <shanbhagshoumik@gmail.com>
2025-08-22 10:50:33 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<nav>
+    <a href="index.php">Home</a> |
+    <a href="about.php">About</a> |
+    <a href="contact.php">Contact</a>
+</nav>
+<hr>
